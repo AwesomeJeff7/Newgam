f43cf349c6ee0bcfce1b4c827bd65d54e5c57218
JEFF <Bulbatar123@gmail.com>
2026-03-09 14:52:52 -0700

    Hide play button and title on click

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ const title = document.getElementById("title");
 playButton.addEventListener("click", function() {
     Skins.hidden = true;
     versionText.hidden = true;
+    playButton.hidden = true;
     title.hidden = true;
 });
