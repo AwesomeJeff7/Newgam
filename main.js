6a8393590ee2b92d9c32996a97d413910e52fc61
JEFF <Bulbatar123@gmail.com>
2026-03-10 18:12:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-const playButton = document.getElementById("playButton");
-const skins = document.getElementById("Skins");
-const versionText = document.getElementById("versionText");
 const title = document.getElementById("title");
-
-document.addEventListener("keydown", function(event) {
-  if (event.code === "Space") {
-    skins.style.display = "none";
-    versionText.style.display = "none";
-    playButton.style.display = "none";
-    title.style.display = "none";
-  }
+playButton.addEventListener("click", function() {
+    Skins.hidden = true;
+    versionText.hidden = true;
+    playButton.hidden = true;
+    title.hidden = true;
 });
