86230ed4a4ee9705a8c4a9fb96e9f36d6409bdf1
JEFF <Bulbatar123@gmail.com>
2026-03-10 18:14:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const title = document.getElementById("title");
+const skins = document.getElementById("skins");
+const versionText = document.getElementById("versionText");
+const playButton = document.getElementById("play");
 playButton.addEventListener("click", function() {
     Skins.hidden = true;
     versionText.hidden = true;
