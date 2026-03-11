b864d930ce9ee239007a69b566fe649260467773
JEFF <Bulbatar123@gmail.com>
2026-03-10 17:56:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const playButton = document.getElementById("playButton");
-const Skins = document.getElementById("Skins");
+const skins = document.getElementById("Skins");
 const versionText = document.getElementById("versionText");
 const title = document.getElementById("title");
 playButton.addEventListener("click", function() {
