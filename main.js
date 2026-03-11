9d2893e3d2c16cb1cb00ffc3bb53a3cf282f1cb5
JEFF <Bulbatar123@gmail.com>
2026-03-10 18:03:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const skins = document.getElementById("Skins");
 const versionText = document.getElementById("versionText");
 const title = document.getElementById("title");
 
-playButton.addEventListener("click", function() {
+document.addEventListener("keydown", function(event) {
+  if (event.code === "Space") {
     skins.style.display = "none";
     versionText.style.display = "none";
     playButton.style.display = "none";
     title.style.display = "none";
+  }
 });
