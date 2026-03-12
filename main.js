47662a955b03ad3b1ca10370f00848958bcc95f2
JEFF <Bulbatar123@gmail.com>
2026-03-12 10:01:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-const title = document.getElementById("title");
-const skins = document.getElementById("Skins");
-const versionText = document.getElementById("versionText");
-const playButton = document.getElementById("playButton");
+document.addEventListener("DOMContentLoaded", function() {
+    const title = document.getElementById("title");
+    const skins = document.getElementById("Skins");
+    const versionText = document.getElementById("versionText");
+    const playButton = document.getElementById("playButton");
 
-function hideMenu() {
-    skins.hidden = true;
-    versionText.hidden = true;
-    playButton.hidden = true;
-    title.hidden = true;
-}
-playButton.addEventListener("click", hideMenu);
-
-document.addEventListener("keydown", function(event) {
-    if(event.code === "Space") {
-        event.preventDefault();
-        hideMenu();
+    function hideMenu() {
+        skins.hidden = true;
+        versionText.hidden = true;
+        playButton.hidden = true;
+        title.hidden = true;
     }
+
+    playButton.addEventListener("click", hideMenu);
+
+    document.addEventListener("keydown", function(event) {
+        if(event.code === "Space") {
+            event.preventDefault();
+            hideMenu();
+        }
+    });
 });
