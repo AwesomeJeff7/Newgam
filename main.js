085cb85ac1d051919fd813d9d1bc3ffae3fd05fc
JEFF <Bulbatar123@gmail.com>
2026-03-16 21:18:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -37,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 startGame();
             } else {
                 clicks++;
+                ScoreText.textContent = "Clicks: " + clicks;
                 console.log(clicks);
             }
         }
@@ -76,6 +77,7 @@ document.addEventListener("DOMContentLoaded", function() {
     player.addEventListener("click", function() {
         if(gameStarted) {
             clicks++;
+            ScoreText.textContent = "Clicks: " + clicks;
             console.log(clicks);
         }
     });
